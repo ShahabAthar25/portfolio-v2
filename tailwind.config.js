95726c48875079a36af76e3d89c2863bbff02f98
ShahabAthar25 <shahabathar25@gmail.com>
2024-04-13 21:24:07 +0500

    Add project

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+    colors: {
+      "text": "#FFFFFF",
+      "background": "#1C2331",
+      "primary": "#2F27CE",
+      "secondary": "#8380B3",
+      "accent": "#37356E"
+    }
+  },
+  plugins: [],
+};
